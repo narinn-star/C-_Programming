ce92588f5e56ee24876a9a9f471d62c61d34c2e9
narinn-star <skfls2618@naver.com>
2021-01-11 23:25:40 +0900

    21.01.11

--- Study/Preview/Chapter02/2-8.cpp
+++ Study/Preview/Chapter02/2-8.cpp
@@ -2,8 +2,20 @@
 #include <string>
 using namespace std;
 
-int main() {
+ int main() {
 	cout << "5명의 이름을 ';'으로 구분하여 입력해주세요.\n>> ";
-	string names;
-	//------------------보류-------------------//
+	char names[100];
+	int max = 0;
+	string result;
+
+	for (int i = 1; i <= 5; i++) {
+		cin.getline(names, 100, ';');
+		cout << i << " : " << names << "\n";
+
+		if (max < strlen(names)) {
+			max = strlen(names);
+			result = names;
+		}
+	}
+	cout << "가장 긴 이름은 " << result;
 }

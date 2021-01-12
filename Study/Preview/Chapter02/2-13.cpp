62298f88899ffa22b4168790ca20d940be44ad36
narinn-star <skfls2618@naver.com>
2021-01-12 23:20:53 +0900

    21.01.12

--- Study/Preview/Chapter02/2-13.cpp
+++ Study/Preview/Chapter02/2-13.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-	while (1) {
+	int a, c;
+	char b;
 
+	while (1) {
+		cout << "? ";
+		cin >> a >> b >> c;
+
+		switch (b) {
+		case '+': cout << a << " " << b << " " << c << " = " << a + c << "\n";
+			break;
+		case '-': cout << a << " " << b << " " << c << " = " << a - c << "\n";
+			break;
+		case '*': cout << a << " " << b << " " << c << " = " << a * c << "\n";
+			break;
+		case '/': cout << a << " " << b << " " << c << " = " << a / c << "\n";
+			break;
+		case '%': cout << a << " " << b << " " << c << " = " << a % c << "\n";
+			break;
+		default: cout << "잘못된 연산자 입니다." << "\n";
+		}
 	}
-	//--------------보류-------------//
 }

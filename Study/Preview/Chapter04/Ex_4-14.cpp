28d0f0b931c7c6254fc2694b9e6a1444e9ee3808
narinn-star <skfls2618@naver.com>
2021-02-02 22:24:14 +0900

    21.02.02

--- Study/Preview/Chapter04/Ex_4-14.cpp
+++ Study/Preview/Chapter04/Ex_4-14.cpp
@@ -13,19 +13,22 @@ int main() {
 	int fIndex;
 	while (true) {
 		fIndex = s.find('+', startIndex); 
+		
 		if (fIndex == -1) { // '+' 문자 발견 X
 			string part = s.substr(startIndex);
 			if (part == "") break; // "2+3+", 즉 +로 끝나는 경우
 			cout << part << endl;
 			sum += stoi(part);
-			break;
+			break; // +만 있으니 더이상 진행 할 필요 X
 		}
-		int count = fIndex - startIndex;
+		
+		int count = fIndex - startIndex; // 자를 문자 개수
 		string part = s.substr(startIndex, count);
 
 		cout << part << endl;
-		sum += stoi(part);
-		startIndex = fIndex + 1;
+		sum += stoi(part); // s => int형으로 바꾸기
+		startIndex = fIndex + 1; // 검색할 인덱스 플러스 시키기
 	}
 	cout << "숫자들의 합은 " << sum;
 }
+// 내가혼자이걸어떻게해ㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐㅐ
